2fb36626dfa60ccdda1622eace5b52be1bac1d04
Adarsh <2017csb1064@iitrpr.ac.in>
2020-05-05 06:37:48 +0530

    Add Promo: Single Page Application-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { ContactComponent } from './contact/contact.component';
 
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,10 @@ import { DishService } from './services/dish.service';
     MatCardModule,
     MatButtonModule,
   ],
-  providers: [DishService,],
+  providers: [
+    DishService,
+    PromotionService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
