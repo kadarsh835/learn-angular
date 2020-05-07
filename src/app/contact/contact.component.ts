1df346d712fda76f4221a4d4ba5a94fa51009e54
Adarsh <2017csb1064@iitrpr.ac.in>
2020-05-08 05:23:54 +0530

    RxJS: Reactive Forms Part 3- Use valueChanges observer

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -15,6 +15,34 @@ export class ContactComponent implements OnInit {
 
   @ViewChild('fform') feedbackFormDirective;
 
+  formErrors= {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': '',
+  };
+
+  validationMessages= {
+    'firstname': {
+      'required': 'Firstname is required.',
+      'minlength': 'Firstname must be atleast 2 characters long.',
+      'maxlength': 'Firstname cannot be more than 25 characters long',
+    },
+    'lastname': {
+      'required': 'Lastname is required.',
+      'minlength': 'Lastname must be atleast 2 characters long.',
+      'maxlength': 'Lastname cannot be more than 25 characters long',
+    },
+    'telnum': {
+      'required': 'Tel. Number is required.',
+      'pattern': 'Tel. number must contain only numbers.',
+    },
+    'email': {
+      'required': 'Email is required.',
+      'email': 'Email not in valid format.',
+    }
+  };
+
   constructor(private fb: FormBuilder) {
     this.createForm();
   }
@@ -24,14 +52,37 @@ export class ContactComponent implements OnInit {
 
   createForm(){
     this.feedbackForm= this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: [0, Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: [0, [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contactType: 'None',
       message: '',
     });
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data))
+
+    this.onValueChanged();  // (Re)set the form validation messages
+  }
+
+  onValueChanged(data?: any){
+    if(!this.feedbackForm){return ;}
+    const form= this.feedbackForm;
+    for (const field in this.formErrors){
+      if(this.formErrors.hasOwnProperty(field)){
+        this.formErrors[field]='';  //clear previous error messages, if any
+        const control= form.get(field)
+        if (control && control.dirty && !control.valid){
+          const messages= this.validationMessages[field];
+          for(const key in control.errors){
+            if(control.errors.hasOwnProperty(key)){
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
   onSubmit(){
