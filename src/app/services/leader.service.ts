06f584b2a6e38413436a202a45fe93ed7b8abfb6
Adarsh <2017csb1064@iitrpr.ac.in>
2020-05-07 22:13:57 +0530

    Angular:Promises Part 2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -10,14 +10,23 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Array<Leader>>{
-    return Promise.resolve(LEADERS);
+    return new Promise(resolve=>{
+      //Simulate 2 sec Server Latency
+      setTimeout(()=>resolve(LEADERS), 2000);
+    });
   }
 
   getLeader(id: string): Promise<Leader>{
-    return Promise.resolve(LEADERS.filter((leader)=> (leader.id===id))[0]);
+    return new Promise(resolve=>{
+      //Simulate 2 sec Server Latency
+      setTimeout(()=>resolve(LEADERS.filter((leader)=> (leader.id===id))[0]), 2000);
+    });
   }
 
   getFeaturedLeader(): Promise<Leader>{
-    return Promise.resolve(LEADERS.filter((leader)=>(leader.featured))[0]);
+    return new Promise(resolve=>{
+      //Simulate 2 sec Server Latency
+      setTimeout(()=>resolve(LEADERS.filter((leader)=>(leader.featured))[0]), 2000);
+    });
   }
 }
